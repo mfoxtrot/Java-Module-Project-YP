837308e18c1f113029eecb0fc95d4d44d3710b9a
mfoxtrot <dmitrey.sokolov@gmail.com>
2023-02-08 13:06:50 +0300

    input data & output result

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 // dev branch for Y.Practicum
+import java.lang.invoke.ConstantCallSite;
+import java.util.Scanner;
 
 class Dish {
     String name;
@@ -24,6 +26,7 @@ class Bill {
     public void addDish(Dish dish) {
         this.listOfDishes = this.listOfDishes + '\n' + dish.name;
         this.totalSum += dish.price;
+        System.out.println("Блюдо " + dish.name + " добавлено в чек");
     }
 
     public void print() {
@@ -38,12 +41,72 @@ class Calculator {
     Calculator(Bill bill) {
         this.bill = bill;
     }
-    public double calculate() {
+    public double payment() {
         return bill.totalSum/bill.numberOfPersons;
+    }
+
+    public String formattedOutPut() {
+        double payment = this.payment();
+        return String.format("%.2f " + this.rubbleProperCase(payment), payment);
+    }
+
+    private String rubbleProperCase(Double sum) {
+        int intSum = (int)Math.floor(sum);
+
+        if (intSum%10==1) {
+            return "рубль";
+        }
+        else if (intSum%10==2 && intSum!=12) {
+            return "рубля";
+        }
+        else if (intSum%10==3 && intSum!=13) {
+            return "рубля";
+        }
+        else if (intSum%10==4 && intSum!=14) {
+            return "рубля";
+        }
+        else {
+            return "рублей";
+        }
     }
 }
 public class Main {
 
     public static void main(String[] args) {
+        Bill bill;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Получаем количество гостей
+        while (true) {
+            System.out.print("На сколько гостей разбить чек? ");
+            int numberOfPersons = scanner.nextInt();
+            if (numberOfPersons>1) {
+                bill = new Bill(numberOfPersons);
+                break;
+            }
+            System.out.println("Неверное значение. Количество гостей должно быть больше 1. Попробуйте еще раз.");
+        }
+        scanner.nextLine();//Вынужденная мера для обхода ошибки https://stackoverflow.com/questions/23450524/java-scanner-doesnt-wait-for-user-input
+        while (true) {
+            System.out.print("Введите название блюда или команду Завершить: ");
+            String inputString = scanner.nextLine();
+            if (inputString.equalsIgnoreCase("завершить")) {
+                break;
+            }
+
+            System.out.print("Введите цену блюда: ");
+            double inputPrice = scanner.nextDouble();
+            Dish dish = new Dish(inputString, inputPrice);
+            bill.addDish(dish);
+
+            scanner.nextLine();//аналогично, комментарию выше
+        }
+
+        bill.print();//выводим счет на экран
+        Calculator calc = new Calculator(bill);
+        System.out.println(calc.formattedOutPut());//выводим итог расчета
+        scanner.close();
     }
 }
+
